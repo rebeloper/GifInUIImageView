fae7070cf7cf2a2941fe33a802afe67f35a8abc5
Alex <therebeloper@gmail.com>
2019-04-08 09:33:57 +0300

    Initial Commit

---
+++ GifInUIImageView/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  GifInUIImageView
+//
+//  Created by Alex Nagy on 08/04/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
